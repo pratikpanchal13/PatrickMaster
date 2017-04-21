68ef2094e64cbb6ea1b95868d9bce7b37ecf061f
pratikpanchal13 <pratik.pancha13@gmail.com>
2017-04-21 13:58:06 +0530

    Optimize code

--- PatrickMaster/Controllers/PhotoVideoVC/PhototVideoVC.swift
+++ PatrickMaster/Controllers/PhotoVideoVC/PhototVideoVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PhototVideoVC: UIViewController,UIScrollViewDelegate {
+class PhototVideoVC: UIViewController {
 
     @IBOutlet weak var imgViewFullScreen: UIImageView!
     var selectedIndexImage : Int?
@@ -20,40 +20,38 @@ class PhototVideoVC: UIViewController,UIScrollViewDelegate {
         
         print("Image Data\(String(describing: selectedIndexImage))")
         print("Index Data\(arrayContentData)")
+      
+        self.setScroll()
+        
+        imgViewFullScreen.image = UIImage(named: arrayContentData[selectedIndexImage!] as! String)
+        
+    }
+
+   
+
+
+    //MARK: -Button Action Event
+    
+    @IBAction func btnBackClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
+
+//MARK: - ScrollView Delegate
+extension PhototVideoVC:UIScrollViewDelegate{
+
+    func setScroll()
+    {
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 3.0
         scrollView.bounces = false
         scrollView.delegate = self
-
-        
-        imgViewFullScreen.image = UIImage(named: arrayContentData[selectedIndexImage!] as! String)
-        
-        
-        // Do any additional setup after loading the view.
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView?
     {
         return imgViewFullScreen
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    @IBAction func btnBackClicked(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
